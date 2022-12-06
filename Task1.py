970df9500c5f115c13a5467937fa9348c4e42d89
Anastasia Zubarenko <zubarana18@gmail.com>
2022-12-06 13:27:04 +0300

    Task1

--- Task1.py
+++ Task1.py
@@ -3,16 +3,12 @@
 *Пример:*
 
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12'''
-from random import random, randint
-N = int(input('Введите число: '))
-numbers = []
-for i in range(N):
-    numbers.append(randint(-N,N+1))
-print(numbers)
-SumOfElements = 0
-for elements in range(len(numbers)):
-    if elements % 2 != 0:
-        SumOfElements += numbers[elements]
-        print(SumOfElements)
-print()
-print(f'Сумма элементов на нечетной позиции равна {SumOfElements}')
+
+def SumOfIndex(numbers):
+    SumOfElements = 0
+    for elements in range(len(numbers)):
+        if elements % 2 != 0:
+            SumOfElements += numbers[elements]
+    print(f'Сумма элементов на нечетной позиции равна {SumOfElements}')
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+SumOfIndex(numbers)
